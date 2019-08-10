6acdd370e163dd830adfe7510e4649129aac6853
Rhett Flanagan <rhettsfp@gmail.com>
2019-08-10 18:28:38 +0200

    Almost done basic sim

---
+++ src/agents.py
@@ -0,0 +1,196 @@
+from mesa import Agent
+import numpy as np
+import math
+
+# Class to setup the agents for the model:
+# Tile: A psudeo agent used primarily as a container for data, can be a river, settlement or field
+# Household: The active agent in the simulation. Dwells in a settlement, and farms fields
+
+class Tile(Agent):
+    """
+    Class implementing the functionality of an patch in a NetLogo.
+
+    Not indended to be used on its own, but to inherit its methods to multiple
+    other agents.
+    """
+
+    # Variable declarations for non python programmer sanity
+    pos = (0,0)
+    fertility = 0.0
+    settlement_territory = False
+    owned = False
+    color = None
+
+    def __init__(self, unique_id, model, pos: tuple):
+        '''
+        Create a new Tile
+
+        Args:
+            pos: Tuple representing the position of the agent on a grid
+            model: The model in which the agent is being used
+        '''
+        super().__init__(unique_id, model)
+        self.pos = pos
+    
+    def step(self):
+        pass
+
+class River(Tile):
+    """
+    River agent, currently does nothing and is used only as an identifier
+    """ 
+    def __init__(self, unique_id, model, pos: tuple):
+        '''
+        Create a new River
+
+        Args:
+            pos: Tuple representing the position of the agent on a grid
+            model: The model in which the agent is being used
+        '''
+        super().__init__(unique_id, model, pos)
+
+
+class Field(Tile):
+    """
+    Fields agent, can be farmed by households and have changing fertility values and owners
+    
+    """
+
+    # Variable declarations for non python programmer sanity
+    avf = 0.0
+    yearsFallow = 0
+
+    def __init__(self, unique_id: int, model, pos: tuple = (0,0), fertility: float = 0.0):
+        '''
+        Create a new Field
+
+        Args:
+            pos: Tuple representing the position of the agent on a grid
+            model: The model in which the agent is being used
+            fertility: The starting fertility of the field
+        '''
+        super().__init__(unique_id, model, pos)
+        self.fertility = fertility
+        self.avf = fertility
+        self.yearsFallow = 0
+    
+    def flood(self):
+        """
+        Changes the fertility value simulating annual flood
+        """
+        mu = self.model.mu
+        #sigma = self.model.sigma
+        alpha = self.model.alpha
+        beta = self.model.beta
+        ticks = self.model.currentTime
+
+        self.fertility = 17 * ( beta * (math.exp(0 - (self.pos[0] - mu) ** 2 /  alpha)))
+        self.avf = ((ticks * self.avf) + self.fertility)/(ticks + 1)
+        self.harvested = False
+    
+    
+
+class Settlement(Tile):
+    """
+    Settlement agent, contains households and has changing population and number of households
+    """
+
+    # Variable declarations for non python programmer sanity
+    population = 0
+    noHouseholds = 0
+
+    def __init__(self, unique_id: int, model, pos: tuple, population: int, noHouseholds: int):
+        '''
+        Create a new Settlement
+
+        Args:
+            pos: Tuple representing the position of the agent on a grid
+            model: The model in which the agent is being used
+            population: The starting population of the setllement (Number of Households * Household Population)
+            noHouseholds: The number of Households in the Settlement
+        '''
+        super().__init__(unique_id, model, pos)
+        self.population = population
+        self.noHouseholds = noHouseholds
+
+class Household(Agent):
+    """
+    Household agent, the active agent in the simulation, contains information relevant to descision making and metrics
+    """
+
+    # Variable declarations for non python programmer sanity
+    pos = (0, 0)
+    grain = 0
+    workers = 0
+    ambition = 0.0
+    competency = 0.0
+    workersWorked = 0
+    generationCountdown = 0
+    fieldsOwned = 0
+    fieldsHarvested = 0
+    fields = []
+
+    def __init__(self, unique_id: int, model, pos: tuple, grain: int,
+                 workers: int, ambition: float, competency: float, 
+                 generationCountdown: int):
+        '''
+        Create a new Settlement
+
+        Args:
+            pos: Tuple representing the position of the agent on a grid
+            model: The model in which the agent is being used
+            grain: The grain that the settlement has
+            workers: The number of workers in the Household
+        '''
+        super().__init__(unique_id, model)
+        self.pos = pos
+        self.grain = grain
+        self.workers = workers
+        self.ambition = ambition
+        self.competency = competency
+        self.generationCountdown = generationCountdown
+        self.fieldsOwned = 0
+        self.fieldsHarvested = 0
+        self.fields = []
+
+    def claimFields(self):
+        """
+        This method allows households to *decide* whether or not to claim fields that fall within their knowledge-radii.
+        The decision to claim is a function the productivity of the field compared to existing fields and ambition.
+        """
+        chance = np.random.uniform(0, 1)
+        if (chance > self.ambition and self.workers > self.ieldsOwned) or (self.fieldsOwned <= 1):
+            bestFertility = 0
+            bestField = None
+
+            # Iterate through fields on the grid
+            neighbours = self.model.grid.get_neighbours(self.pos, False, False, self.model.knowledgeRadius)
+            for a in neighbours:
+                if (a.fertility > bestFertility and type(a).__name__ == "Field" 
+                    and a.owned == False and a.settlement_territory == False):
+                    bestFertility = a.fertility
+                    bestField = a
+            
+            # Make claim
+            if bestField != None:
+                # Redundancy checks
+                if(type(bestField).__name__ == "Field" and bestField.owned == False 
+                    and bestField.settlement_territory == False):
+                    bestField.owned = True
+                    bestField.harvested = False
+                    bestField.yearsFallow = 0
+                    self.fieldsOwned += 1
+                    self.fields.append(bestField)
+    
+    def farm(self):
+        #total
+
+    def rent(self):
+        # TODO
+        pass
+    
+    def consumeGrain(self):
+        pass
+
+    
+
---
+++ src/model.py
@@ -0,0 +1,196 @@
+from mesa import Model
+from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
+import numpy as np
+import random
+import math
+
+from src.agents import River, Field, Settlement, Household
+from src.schedule import RandomActivationByBreed
+
+class EgyptSim(Model):
+    """
+    Simulation Model for wealth distribution represented by grain in ancient Egypt
+    """
+
+    # Variable declarations for non python programmer sanity
+    # Map variables
+    height = 30
+    width = 30
+
+    # Simulation Variables
+    timeSpan = 500
+    currentTime = 0
+    startingSettlements = 14
+    startingHouseholds = 7
+    startingHouseholdSize = 5
+    startingGrain = 3000
+    minAmbition = 0.1
+    minCompetency = 0.5
+    generationalVariation = 0.9
+    knowledgeRadius = 20
+    distanceCost = 10
+    fallowLimit = 4
+    popGrowthRate = 0.1
+    fission = False
+    fissionChance = 0.7
+    rental = False
+    rentalRate = 0.5
+
+    # Step variables
+    mu = 0
+    sigma = 0
+    alpha = 0
+    beta = 0
+
+    # Debug
+    verbose = False
+
+    # Visualisation
+    description = "A model simulating wealth growth and distribution in Ancient Egypt"
+
+    def __init__(self, height: int = 30, width: int = 30, timeSpan: int = 500,
+                startingSettlements: int = 14, startingHouseholds: int = 7,
+                startingHouseholdSize: int = 5, startingGrain: int = 3000,
+                minAmbition: float = 0.1, minCompetency: float = 0.5,
+                generationalVariation: float = 0.9, knowledgeRadius: int = 20,
+                distanceCost: int = 10, fallowLimit: int = 4, popGrowthRate: float = 0.1,
+                fission: bool = False, fissionChance: float = 0.7, rental: bool = False,
+                rentalRate: float = 0.5):
+        """
+        Create a new EgyptSim model
+        Args:
+            height: The height of the simulation grid
+            width: The width of the simulation grid
+            timeSpan: The number of years over which the model is to run
+            startingSettlements: The starting number of Settlements 
+            startingHouseholds: The starting number of Households per Settlement
+            startingHouseholdSize: The starting number of workers in a Household
+            startingGrain: The starting amount of grain for each Household
+            minAmbition: The minimum ambition value for a Household
+            minCompetency: The minimum competency value for a Household
+            generationalVariation: The difference between generations of a Household
+            knowledgeRadius: How far outside ther Settlement a Household can "see"
+            distanceCost: The cost to move grain per cell away from a settlemnt
+            fallowLimit: The number of years a field can lay fallow before it is harvested
+            popGrowthRate: The rate at which the population grows
+            fission: If Household fission (Moving between settlements) is allowed
+            fissionChance: The chance fission occuring
+            rental: If land rental is allowed
+            rentalRate: The rate at which households will rent land
+        """
+        super().__init__()
+        # Set Parameters
+        # Map variables
+        self.height = height
+        self.width = width
+
+        # Simulation Variables
+        self.timeSpan = timeSpan
+        self.currentTime = 0
+        self.startingSettlements = startingSettlements
+        self.startingHouseholds = startingHouseholds
+        self.startingHouseholdSize = startingHouseholdSize
+        self.startingGrain = startingGrain
+        self.minAmbition = minAmbition
+        self.minCompetency = minCompetency
+        self.generationalVariation = generationalVariation
+        self.knowledgeRadius = knowledgeRadius
+        self.distanceCost = distanceCost
+        self.fallowLimit = fallowLimit
+        self.popGrowthRate = popGrowthRate
+        self.fission = fission
+        self.fissionChance = fissionChance
+        self.rental = rental
+        self.rentalRate = rentalRate
+
+        self.schedule = RandomActivationByBreed(self)
+        self.grid = MultiGrid(self.height, self.width, torus = False)
+        # TODO Setup full data collection
+        self.datacollector = DataCollector(
+            {"Households": lambda m: m.schedule.get_breed_count(Household),
+             "Settlements": lambda m: m.schedule.get_breed_count(Settlement),
+            })
+
+        self.setup()
+    
+    def setupMapBase(self):
+        """
+        Create the grid as field and river
+        """
+        for agent, x, y in self.grid.coord_iter():
+            # If on left edge, make a river
+            if x == 0:
+                river = River(self.next_id(), self, (x, y))
+                self.grid.place_agent(river, (x, y))
+            # Otherwise make a field
+            else:
+                field = Field(self.next_id(), self, (x, y), 0.0)
+                self.grid.place_agent(field, (x, y))
+                self.schedule.add(field)
+
+    def setupSettlementsHouseholds(self):
+        """
+        Add settlements and households to the simulation
+        """
+        taken = []
+        for i in range(self.startingSettlements):
+            # Loop untill a suitable location is found
+            while True:
+                x = self.random.randrange(1, self.width)
+                y = self.random.randrange(self.height)
+
+                # Check if there is a sellment at the location
+                flag = True
+                for loc in taken:
+                    if loc == (x, y):
+                        flag = False
+                        break
+                # If there is no settlement, proceed, otherwise relocate coordinates
+                if flag:
+                    taken.append((x, y))
+                    break
+
+            # Add settlement to the grid
+            population = self.startingHouseholds * self.startingHouseholdSize
+            settlement = Settlement(self.next_id(), self, (x, y), population, self.startingHouseholds)
+            self.grid.place_agent(settlement, (x, y))
+
+            # Set the surrounding fields as territory
+            local = self.grid.get_neighbors((x, y), True, True, 1)
+            for a in local:
+                #if type(a).__name__ != "Household":
+                a.settlmentTeritory = True
+            #self.schedule.add(settlement)
+
+            # Add households for the settlement to the scheduler
+            for j in range(self.startingHouseholds):
+                ambition = self.minAmbition + np.random.uniform(0, 1 - self.minAmbition)
+                competency = self.minCompetency + np.random.uniform(0, 1 - self.minCompetency)
+                genCount = self.random.randrange(5)
+                household = Household(self.next_id(), self, (x, y), self.startingGrain,
+                                      self.startingHouseholdSize, ambition, competency,
+                                      genCount)
+                #! Dont add household grid
+                #self.grid.place_agent(household, (x, y))
+                self.schedule.add(household)
+
+    def setup(self):
+        """
+        Setup model parameters
+        """
+        self.setupSettlementsHouseholds()  
+        self.setupMapBase()
+
+    def step(self):
+        self.currentTime += 1
+        self.setupFlood()
+    
+    def setupFlood(self):
+        """
+        Variables used for flood methods for fields
+        """
+        self.mu = random.randint(0, 11) + 5
+        self.sigma = random.randint(0, 6) + 5
+        self.alpha = (2 * self.sigma ** 2)
+        self.beta = 1/(self.sigma * math.sqrt( 2 * math.pi))
